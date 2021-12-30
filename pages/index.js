939ea82c4a965497d3f54d286149df0d6bb2f7bc
bolingboling <zhangxiaoshang66@163.com>
2021-12-30 21:25:09 +0800

    preview

--- pages/index.js
+++ pages/index.js
@@ -12,6 +12,7 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
+        <h2>Preview version</h2>
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{" "}
